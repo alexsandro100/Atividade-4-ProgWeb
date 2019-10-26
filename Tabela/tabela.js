362e3c246b415bf5c76d32c4d51e0551db989c69
alexsandro100 <asgmj100@gmail.com>
2019-10-26 00:16:53 -0300

    second commit

--- Tabela/tabela.js
+++ Tabela/tabela.js
@@ -1,24 +1,20 @@
-function addZero(num) {
-    return num < 10 ? `0${num}` : num
-}
+function adicionarLinha() {
+    let linhas = document.getElementById('quantidade').value
+    
+    for(let i = 1; i <= linhas; i++) {
+        let nova_linha = table.insertRow(table.length)
+        cell1 = nova_linha.insertCell(0)
+        cell2 = nova_linha.insertCell(1)
+        cell3 = nova_linha.insertCell(2)
+        cell4 = nova_linha.insertCell(3)
+        time = 'Time ' + Math.floor(Math.random() * 100)
+        jogos = Math.floor(Math.random() * 100)
+        pontos = Math.floor(Math.random() * 100)
+        remover = document.getElementById('remover').value
 
-function adicionarLinha(){
-    let qtd_linhas = document.getElementById('quantidade')
-
-    for(let i = 1; i <= qtd_linhas; i++){
-        let nova_linha = table.insertRow(table.length),
-            cell1 = nova_linha.insertCell(1),
-            cell2 = nova_linha.insertCell(2),
-            cell3 = nova_linha.insertCell(3),
-            cell4 = nova_linha.insertCell(4),
-            time = 'time' + addZero(Math.floor(Math.random() * 100)),
-            jogos = addZero(Math.floor(Math.random() * 100)),
-            pontos = addZero(Math.floor(Math.random() * 100));
-            remover = document.getElementById('remover');
-
-        cell1.innerHTML = time;
-        cell2.innerHTML = jogos;
-        cell3.innerHTML = pontos;
-        cell4.innerHTML = `<button onClick="this.parentNode.parentNode.remove();">remover</button>`;
+        cell1.innerHTML = time
+        cell2.innerHTML = jogos
+        cell3.innerHTML = pontos
+        cell4.innerHTML = `<button onClick="this.parentNode.parentNode.remove();">X</button>`
     }
 }
